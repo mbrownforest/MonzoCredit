4b1e0b2ae1f50dc60dfffb8fa7a295535e9dc9de
mbrownforest <43621400+mbrownforest@users.noreply.github.com>
2019-11-26 22:38:41 +0000

    Update Config.java
    remove all of my personal auth details from the internet

--- app/src/main/java/com/example/tescotomonzo/Config.java
+++ app/src/main/java/com/example/tescotomonzo/Config.java
@@ -4,9 +4,9 @@ import java.math.BigInteger;
 
 public class Config {
 
-    public static final String CLIENT_ID = "user_00009aPLzYjztFezLUcnGD";
- //   public static final String CLIENT_SECRET = [your client secret]
-  //  public static final String REDIRECT_URI =
+    public static final String CLIENT_ID = [your client id] //needs to be setup as monzo auth client
+    public static final String CLIENT_SECRET = [your client secret] //needs to be setup as monzo auth client
+    public static final String REDIRECT_URI = [ your redirect uri] //needs to be setup as monzo auth client
     public static final int SESSION_SECRET = 1234;
     public static final int STATE_SECRET = 1234;
 
