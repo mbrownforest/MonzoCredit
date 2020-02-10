9d4385e52098dc57f877f9077f3432e9695fdbed
mbrown <mbrown@whiteclarkegroup.com>
2020-02-10 17:56:00 +0000

    switch back to prior xml

--- app/src/main/java/com/example/tescotomonzo/MonzoViewPotsActivity.java
+++ app/src/main/java/com/example/tescotomonzo/MonzoViewPotsActivity.java
@@ -5,28 +5,41 @@ import android.content.Context;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MonzoViewPotsActivity extends Activity {
 
     TextView monzoPot;
     Button deletePot;
     Context context;
+    Button back;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_pots);
+
         context = this;
 
-        UserCreditValues userCreditValues = new UserCreditValues();
         monzoPot = (TextView) findViewById(R.id.monzo_view_pot);
-        monzoPot.setText(userCreditValues.getMonzoPotName(context));
-
         deletePot = (Button) findViewById(R.id.delete_pot);
-        deletePot.setOnClickListener(v -> {
-            userCreditValues.setMonzoPotName(context, null);
-            userCreditValues.setCreditCardNotification(context, null);
-        });
+
+        ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.view_pots_layout);
+        layout.removeView(deletePot);
+        UserCreditValues userCreditValues = new UserCreditValues();
+        String monzoPotName = userCreditValues.getMonzoPotName(context);
+        if (monzoPotName != null) {
+            monzoPot.setText(userCreditValues.getMonzoPotName(context));
+            layout.addView(deletePot);
+            deletePot.setOnClickListener(v -> {
+                userCreditValues.setMonzoPotName(context, null);
+                userCreditValues.setCreditCardNotification(context, null);
+            });
+        } else {
+            monzoPot.setText("You do not have any Monzo pots");
+        }
 
     }
+
 }
